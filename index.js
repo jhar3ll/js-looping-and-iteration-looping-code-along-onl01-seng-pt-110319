0eeeb7863b652063c0e0f466d058e15da3d91daf
jhar3ll <jharell.chambers@selu.edu>
2020-06-03 14:54:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards( gifters, event ) {
+    let cards = []
+    for ( let i = 0; i < gifters.length; i++ ) {
+     cards.push( `Thank you, ${gifters[i]}, for the wonderful ${event} gift!` )
+    }
+    return cards
+  }
+
+  
+ function countDown (beginNumber) {
+     while (beginNumber >= 0) {
+         console.log(beginNumber--);
+     }
+ }
